3aad7becb821a033532dd2b8b741082588a2198c
Josh <josh.haddix598@gmail.com>
2022-07-20 22:15:47 -0500

    generate page with promises

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 
 const generatePage = require('./src/page-template.js');
 
+const { writeFile, copyFile } = require('./utils/generate-site');
+
+
 /*const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [name,github] = profileDataArgs;
@@ -162,12 +165,19 @@ const promptProject = portfolioData => {
 
 promptUser()
     .then(promptProject)
-    .then(portfolioData => {
-         const pageHTML = generatePage(portfolioData);
-
-         fs.writeFile('./index.html', pageHTML, err => {
-           if (err) throw new Error(err);
-
-           console.log('Page created! Check out index.html in this directory to see it!');
-         });
-    });
+        .then(portfolioData => {
+            return generatePage(portfolioData);
+        })
+        .then(pageHTML => {
+            return writeFile(pageHTML);
+        })
+        .then(writeFileResponse => {
+            console.log(writeFileResponse);
+            return copyFile();
+        })
+        .then(copyFileResponse => {
+            console.log(copyFileResponse);
+        })
+        .catch(err => {
+            console.log(err)
+        })
